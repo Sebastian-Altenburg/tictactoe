85307bf067580e1f97c3f574e28e656ab86a50da
sebastian-altenburg <saltenburg90@gmail.com>
2020-10-15 16:00:58 +0200

    completed game logic, added scoring, game is working

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let setup = (()=>{
     let restartGame = document.querySelector(".restartGame");
     let restartButton = document.querySelector("#restartButton");
 
+    
+    let score = document.querySelector("#score");
+    let score2 = document.querySelector("#score2");
+
     restartButton.addEventListener("click", ()=> {
         location.reload();
     });
@@ -24,8 +28,10 @@ let setup = (()=>{
         ["","",""],
         ["","",""]
     ];
+
     
-    return {ctx, myCanvas, gameBoard, restartButton, restartGame};
+    
+    return {ctx, myCanvas, gameBoard, restartButton, restartGame, score,score2};
 })();
 
 /////////// Players
@@ -60,7 +66,8 @@ let playerX = players(0, true);
 let playerO = players(0, false);
 
 //////////Setting up the grid
-let drawGrid = (()=>{
+let setupGrid = (()=>{
+    
     let width = setup.myCanvas.width;
     let height = setup.myCanvas.height;
     for(i = 0; i < 4; i++) {
@@ -72,6 +79,20 @@ let drawGrid = (()=>{
         setup.ctx.lineWidth = 6;
         setup.ctx.stroke();
     }
+
+    let draw = ()=> {
+        for(i = 0; i < 4; i++) {
+            setup.ctx.beginPath();
+            if(i==0) {setup.ctx.moveTo(width/3,0);setup.ctx.lineTo(width/3,height);}
+            if(i==1) {setup.ctx.moveTo(width*2/3,0);setup.ctx.lineTo(width*2/3,height);}
+            if(i==2) {setup.ctx.moveTo(0,height/3);setup.ctx.lineTo(width,height/3);}
+            if(i==3) {setup.ctx.moveTo(0,height*2/3);setup.ctx.lineTo(width,height*2/3);}
+            setup.ctx.lineWidth = 6;
+            setup.ctx.stroke();
+        }
+    }
+
+    return{draw};
 })();
 
 
@@ -120,6 +141,17 @@ function swapTurns(){
     else if (playerX.turn == false)      {playerX.turn = true};
 }
 ///////////////////////
+function clearBoard() {
+    
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++){
+            setup.gameBoard[j][i] = "";
+        }
+    }
+    setup.ctx.clearRect(0, 0, 1000,1000);
+    setupGrid.draw();
+}
+//////////////////////
 function gameLogic(){
     if ((setup.gameBoard[0][0] == "X" && setup.gameBoard[0][1] == "X" && setup.gameBoard[0][2] == "X")    || 
         (setup.gameBoard[1][0] == "X" && setup.gameBoard[1][1] == "X" && setup.gameBoard[1][2] == "X")    ||
@@ -131,10 +163,24 @@ function gameLogic(){
         (setup.gameBoard[0][2] == "X" && setup.gameBoard[1][1] == "X" && setup.gameBoard[2][0] == "X")) {
 
             console.log("Player X wins!");
-            setup.myCanvas.classList.add("hide");
-            setup.restartGame.classList.remove("hide");
-            setup.restartGame.classList.add("showDiv");
-            setup.restartButton.classList.remove("hide");
+
+            playerX.score++;
+
+            if (playerX.score == 3) {
+                
+
+                setup.myCanvas.classList.add("hide");
+                setup.restartGame.classList.remove("hide");
+                
+                setup.restartGame.classList.add("showDiv");
+                setup.restartButton.classList.remove("hide");
+                setup.restartButton.innerHTML = "Player X Wins!";
+            }
+            else {
+                clearBoard();
+            }
+
+            
     }
     else if (   (setup.gameBoard[0][0] == "O" && setup.gameBoard[0][1] == "O" && setup.gameBoard[0][2] == "O")    || 
                 (setup.gameBoard[1][0] == "O" && setup.gameBoard[1][1] == "O" && setup.gameBoard[1][2] == "O")    ||
@@ -146,9 +192,55 @@ function gameLogic(){
                 (setup.gameBoard[0][2] == "O" && setup.gameBoard[1][1] == "O" && setup.gameBoard[2][0] == "O")) {
         
                     console.log("Player O wins!");
-                    setup.myCanvas.classList.add("hide");
-                    setup.restartGame.classList.remove("hide");
-                    setup.restartGame.classList.add("showDiv");
-                    setup.restartButton.classList.remove("hide");
+
+                    playerO.score++;
+
+                    if(playerO.score == 3) {
+                        
+
+                        setup.myCanvas.classList.add("hide");
+                        setup.restartGame.classList.remove("hide");
+                    
+                        setup.restartGame.classList.add("showDiv");
+                        setup.restartButton.classList.remove("hide");
+                        setup.restartButton.innerHTML = "Player O Wins!";
+                    }
+                    else {
+                        clearBoard();
+                    }
+
+                    
     }   
+
+
+    ////NOT WORKING YET
+    else if(allMarkersSet()){
+        alert("TIED");
+        clearBoard();
+    }
 }
+function allMarkersSet() {  
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j<3; j++){
+            if (setup.gameBoard[i][j] == "") {
+                console.log("allmarkersset is working");
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+////////
+      
+
+
+function update() {
+  setup.score.innerHTML = playerX.score;
+  setup.score2.innerHTML = playerO.score;
+
+
+
+  window.requestAnimationFrame(update);
+}
+update();
